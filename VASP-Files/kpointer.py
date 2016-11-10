301b1e2fe8cb850cf82079ddc404262fc837029f
mtdg-wagner <monsieurwagner@gmail.com>
2016-11-10 13:46:34 -0600

    New file for generating static kpoint meshes
    Creates KPOINTS.static files for VASP calculations of density of states

---
+++ VASP-Files/kpointer.py
@@ -0,0 +1,24 @@
+#!/home/naw356/bin/python
+import argparse, os.path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", nargs='+', action="store",
+        dest="kpoints", default="6_6_6_G",
+        help='''Specify three k point subdivisions
+        and mesh type separated by underscores: 
+        e.g. 6_6_6_G or 4_2_4_M''')
+args = parser.parse_args()
+params = args.kpoints.split('_')
+if params[3] == 'M':
+    mesh = 'Monkhorst-Pack mesh'
+else:
+    mesh = 'Gamma centered mesh'
+
+with open('KPOINTS.stati', 'w') as f:
+    f.write('''Automatic mesh
+0          ! Auto k-point generation scheme
+%s          ! %s 
+%s %s %s      ! Dimensions''' % (params[3], mesh, params[0], params[1],
+    params[2]))
+
+
