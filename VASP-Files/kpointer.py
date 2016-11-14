08a422d11e7b7ef650f4be9c9833d91802cbe955
mtdg-wagner <monsieurwagner@gmail.com>
2016-11-13 20:08:41 -0600

    Add files via upload
    Edited to run differently as module versus as main process

--- VASP-Files/kpointer.py
+++ VASP-Files/kpointer.py
@@ -1,25 +1,32 @@
-#!/home/naw356/bin/python
+#!/opt/apps/intel15/python/2.7.12/bin/python
 # Put your Python executable location on the first line
 import argparse, os.path
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-n", nargs='+', action="store",
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", nargs='+', action="store",
         dest="kpoints", default="6_6_6_G",
         help='''Specify three k point subdivisions
         and mesh type separated by underscores: 
         e.g. 6_6_6_G or 4_2_4_M''')
-args = parser.parse_args()
-params = args.kpoints.split('_')
-if params[3] == 'M':
-    mesh = 'Monkhorst-Pack mesh'
-else:
-    mesh = 'Gamma centered mesh'
+    args = parser.parse_args()
+    params = args.kpoints.split('_')
+    if params[3] == 'M':
+        mesh = 'Monkhorst-Pack mesh'
+    else:
+        mesh = 'Gamma centered mesh'
 
-with open('KPOINTS.static', 'w') as f:
-    f.write('''Automatic mesh
+    with open('KPOINTS.static', 'w') as f:
+        f.write('''Automatic mesh
 0          ! Auto k-point generation scheme
 %s          ! %s 
 %s %s %s      ! Dimensions''' % (params[3], mesh, params[0], params[1],
-    params[2]))
+        params[2]))
 
-
+else:
+    with open('KPOINTS.static', 'w') as f:
+        f.write('''Automatic mesh
+0          ! Auto k-point generation scheme
+%s          ! %s
+%s %s %s      ! Dimensions''' % ('G', 'Gamma centered mesh', 8, 8,
+        8))
