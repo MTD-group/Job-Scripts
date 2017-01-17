5282bc21ba3077cff4c9ad234e1a37a852ba166b
mtdg-wagner <monsieurwagner@gmail.com>
2017-01-17 15:43:09 -0600

    Update bsdos_plotter.py
    Added automatic output of band gap to file named 'bandgap.txt' and additional print statement for this

--- VASP-Files/bsdos_plotter.py
+++ VASP-Files/bsdos_plotter.py
@@ -1,6 +1,7 @@
 # Call with your python executable to generate combined DOS/bandstructure figure 
 #   from your two vasprun files
 # Relies upon pymatgen version 4.5.6 or greater, matplotlib, and glob
+# Also outputs the size and type of bandgap to 'bandgap.txt' and prints this to std out
 
 from pymatgen.io.vasp.outputs import Vasprun
 from pymatgen.electronic_structure.plotter import BSDOSPlotter
@@ -17,6 +18,9 @@ dos_vrun = Vasprun(dos_name)
 bs = bs_vrun.get_band_structure(kpoints_filename = 'KPOINTS.bands')
 dos = dos_vrun.complete_dos
 
+with open('bandgap.txt', 'w') as f:
+    f.write('Bandgap: ' + str(bs.get_band_gap()))
+print('Bandgap: ' + str(bs.get_band_gap()))
 bsdosplot = BSDOSPlotter()
 figuro = bsdosplot.get_plot(bs, dos)
 figuro.savefig('bsdos.png')
