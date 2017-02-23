bd1fe4f488d90739f7a1173454f0908853ca49b3
mtdg-wagner <monsieurwagner@gmail.com>
2017-02-23 12:53:27 -0600

    Update cjob.py

--- Carbon/cjob.py
+++ Carbon/cjob.py
@@ -15,7 +15,7 @@ parser.add_argument("-n", action="store",
         in hours separated by underscores: e.g. Num_Name_Time. 
         4_jobname_8 is default''')
 parser.add_argument("-jt", action='store', dest='jtype', default='static',
-        help='''Job type. 'static' and 'relax' are current options''')
+        help='''Job type. 'static', 'relax', and 'elast' are current options''')
 parser.add_argument("-isif", action='store', dest='isif', default=3,
         help='''Relaxation scheme. 3 is default''')
 
@@ -33,8 +33,8 @@ if not os.path.isfile('POTCAR'):
 if not os.path.isfile('KPOINTS.static'):
     import kpointer
 
-# If KPOINTS.bands is not found, write one
-if not os.path.isfile('KPOINTS.bands'):
+# If KPOINTS.bands is not found bu called for, write one
+if not os.path.isfile('KPOINTS.bands') and jtype != 'elast':
     os.system("~/bin/kpath")
 
 # Store ENCUT value for changing later
@@ -60,6 +60,7 @@ with open('run_vasp.job', 'w') as f:
 static_text = '''sed -i "s/NSW = .*/NSW = 0 # number of ionic steps/" INCAR.static
 sed -i "s/LCHARG = .FALSE./LCHARG = .TRUE./" INCAR.static
 sed -i "s/ISMEAR = .*/ISMEAR = -5/" INCAR.static
+sed -i "s/LREAL = .*/LREAL = .FALSE./" INCAR*
 cp INCAR.static INCAR
 cp KPOINTS.static KPOINTS
 mpirun -machinefile $PBS_NODEFILE -np $PBS_NP vasp > out.static
@@ -70,7 +71,6 @@ sed "s/ICHARG = .*/ICHARG = 11/" INCAR.static > INCAR.bands
 sed -i "s/ISMEAR = .*/ISMEAR = 0/" INCAR.bands
 sed -i "s/.*SIGMA = .*/SIGMA = 0.1/" INCAR.bands
 sed -i "s/.*LCHARG = .*/LCHARG = .FALSE./" INCAR.bands
-sed -i "s/LREAL = .*/LREAL = .FALSE./" INCAR*
 cp KPOINTS.bands KPOINTS
 cp INCAR.bands INCAR
 mpirun -machinefile $PBS_NODEFILE -np $PBS_NP vasp > out.bands
@@ -107,6 +107,18 @@ cp CONTCAR CONTCAR.is%s.ib1.$it
 cp CONTCAR POSCAR
 done\n\n'''
 
+# Elast text is similar to static but we don't calculate band structure or save the CHGCAR
+# We also leave the vasprun alone rather than renaming it.
+elast_text = '''sed -i "s/NSW = .*/NSW = 0 # number of ionic steps/" INCAR.static
+sed -i "s/LCHARG = .FALSE./LCHARG = .TRUE./" INCAR.static
+sed -i "s/ISMEAR = .*/ISMEAR = -5/" INCAR.static
+sed -i "s/LREAL = .*/LREAL = .FALSE./" INCAR*
+cp INCAR.static INCAR
+cp KPOINTS.static KPOINTS
+mpirun -machinefile $PBS_NODEFILE -np $PBS_NP vasp > out.static
+mv OUTCAR OUTCAR.static
+rm CHGCAR WAVECAR\n'''
+
 #Commands for calculating DOS and band structure
 if args.jtype == 'static':
     with open('run_vasp.job', 'a') as f:
@@ -123,3 +135,14 @@ if args.jtype == 'relax':
         a.extend([isif]*12)
         f.write(relax_text % tuple(a))
         f.write(static_text % (params[1], params[1]))
+        
+if args.jtype == 'elast'
+    rel_encut = str(int(encut)*0.9)
+    isif = 2
+    with open('run_vasp.job', 'a') as f:
+        # Format relax_text with isif number and encut number
+        a = [isif]*9
+        a.append(rel_encut)
+        a.extend([isif]*12)
+        f.write(relax_text % tuple(a))
+        f.write(elast_text % (params[1], params[1]))
