0135214a84c04d1348d97fec383929cef792dea3
Nicholas Wagner <monsieurwagner@gmail.com>
2017-03-21 11:39:12 -0500

    Uses Mueller's k-point grid server
    Updated script to generate DOS calculation KPOINTS files using the Mueller group K-Point grid server.

--- VASP-Files/kpointer.py
+++ VASP-Files/kpointer.py
@@ -1,8 +1,9 @@
-#!/opt/apps/intel15/python/2.7.12/bin/python
-# Generates a KPOINTS file for DOS calculation
-# Put your Python executable location on the first line
-import argparse, os.path
+#!/home/naw356/bin/python
+# Script to generate KPOINTS file for SCF calculations
+# Requires PRECALC and getKPoints files to be located in ~/bin/ 
+import argparse, subprocess, shutil, os.path
 
+# Can be called directly to write a VASP automatic scheme KPOINTS file
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", nargs='+', action="store",
@@ -12,7 +13,7 @@ if __name__ == '__main__':
         e.g. 6_6_6_G or 4_2_4_M''')
     args = parser.parse_args()
     params = args.kpoints.split('_')
-    if params[3] == 'M':
+    if params[3].upper() == 'M':
         mesh = 'Monkhorst-Pack mesh'
     else:
         mesh = 'Gamma centered mesh'
@@ -24,10 +25,11 @@ if __name__ == '__main__':
 %s %s %s      ! Dimensions''' % (params[3], mesh, params[0], params[1],
         params[2]))
 
+# If called from elsewhere, will use the Mueller group's K-Point Grid Server
+# Please cite:  Pandu Wisesa, Kyle A. McGill, and Tim Mueller. 
+#               Phys. Rev. B 93, 155109 (2016)
 else:
-    with open('KPOINTS.static', 'w') as f:
-        f.write('''Automatic mesh
-0          ! Auto k-point generation scheme
-%s          ! %s
-%s %s %s      ! Dimensions''' % ('G', 'Gamma centered mesh', 8, 8,
-        8))
+    home = os.path.expanduser("~/bin")
+    shutil.copyfile(home + "/PRECALC", "./PRECALC")
+    subprocess.call(home + "/getKPoints", shell=True)
+    shutil.copyfile("./KPOINTS", "./KPOINTS.static")
